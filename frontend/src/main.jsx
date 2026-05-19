6de70df7fe98d0382aef3aa8034436cf03b69c48
F4chini <f4xcore@gmail.com>
2026-05-19 14:37:48 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,17 +13,22 @@ import Receber from "./pages/Receber";
 import Pagar from "./pages/Pagar";
 import Historico from "./pages/Historico";
 import Contas from "./pages/Contas";
+import Login from "./pages/Login";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Dashboard />} />
-        <Route path="receber" element={<Receber />} />
-        <Route path="pagar" element={<Pagar />} />
-        <Route path="historico" element={<Historico />} />
-        <Route path="contas" element={<Contas />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <Routes>
+
+    <Route path="/login" element={<Login />} />
+
+    <Route path="/" element={<App />}>
+      <Route index element={<Dashboard />} />
+      <Route path="receber" element={<Receber />} />
+      <Route path="pagar" element={<Pagar />} />
+      <Route path="historico" element={<Historico />} />
+      <Route path="contas" element={<Contas />} />
+    </Route>
+
+  </Routes>
+</BrowserRouter>
 );
