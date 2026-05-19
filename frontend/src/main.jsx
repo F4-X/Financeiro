f89d9819a1e8736b18df87654889b6e487c81833
F4chini <f4xcore@gmail.com>
2026-05-19 17:39:13 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,24 +11,36 @@ import App from "./App";
 import Dashboard from "./pages/Dashboard";
 import Receber from "./pages/Receber";
 import Pagar from "./pages/Pagar";
-import Historico from "./pages/Historico";
+import HistoricoPagar from "./pages/HistoricoPagar";
+import HistoricoReceber from "./pages/HistoricoReceber";
 import Contas from "./pages/Contas";
 import Login from "./pages/Login";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Routes>
+    <Routes>
 
-    <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
-    <Route path="/" element={<App />}>
-      <Route index element={<Dashboard />} />
-      <Route path="receber" element={<Receber />} />
-      <Route path="pagar" element={<Pagar />} />
-      <Route path="historico" element={<Historico />} />
-      <Route path="contas" element={<Contas />} />
-    </Route>
+      <Route path="/" element={<App />}>
+        <Route index element={<Dashboard />} />
 
-  </Routes>
-</BrowserRouter>
+        <Route path="receber" element={<Receber />} />
+        <Route path="pagar" element={<Pagar />} />
+
+        <Route
+          path="historico-receber"
+          element={<HistoricoReceber />}
+        />
+
+        <Route
+          path="historico-pagar"
+          element={<HistoricoPagar />}
+        />
+
+        <Route path="contas" element={<Contas />} />
+      </Route>
+
+    </Routes>
+  </BrowserRouter>
 );
